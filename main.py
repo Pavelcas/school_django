5db395297a4a3892f22721028260eb7d3b009b45
Pavelcas <monkeydpavel333@gmail.com>
2022-11-03 14:30:39 -0300

    validate2.0

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def validate(n):
-    n = [int(i) for i in str(n)]
-    for i in range(len(list(reversed(n)))):
-        if i % 2 == 1 and n[i]<9:
+    n = list(reversed([int(i) for i in str(n)]))
+    for i in range(len(n)):
+        if i % 2 == 1 and n[i]<5:
             n[i] = n[i]*2
+        elif i % 2 == 1 and n[i]>=5:
+            n[i] = n[i]*2-9
     return sum(n)%10 == 0
-print(validate(2121))
+print(validate(44116824277224))
